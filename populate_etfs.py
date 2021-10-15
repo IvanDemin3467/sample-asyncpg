f0ee2d2abf1f9e79e378a68ed28abc81e8331220
IvanDemin3467 <IvanDemin3467@gmail.com>
2021-10-15 18:42:45 +0600

    Initial commit after fork

--- populate_etfs.py
+++ populate_etfs.py
@@ -1,6 +1,5 @@
 import config
 import csv
-import psycopg2
 import psycopg2.extras
 
 connection = psycopg2.connect(host=config.DB_HOST, database=config.DB_NAME, user=config.DB_USER, password=config.DB_PASS)
